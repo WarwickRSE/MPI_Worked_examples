e931fada745e371549151ca0eb5d2a92ae1042a8
Chris Brady <C.S.Brady@warwick.ac.uk>
2017-12-04 15:07:06 +0000

    Fixed switched coordinates in parallel case study

--- Fortran/Case_Study/01_Simple/parallel.f90
+++ Fortran/Case_Study/01_Simple/parallel.f90
@@ -157,17 +157,17 @@ MODULE display
     CALL MPI_Comm_rank(cart_comm, rank, ierr)
 
     !Get the rank of the neighbouring processors in Cartesian communicator
-    CALL MPI_Cart_shift(cart_comm, 1, 1, x_min_rank, x_max_rank, ierr)
-    CALL MPI_Cart_shift(cart_comm, 0, 1, y_min_rank, y_max_rank, ierr)
+    CALL MPI_Cart_shift(cart_comm, 0, 1, x_min_rank, x_max_rank, ierr)
+    CALL MPI_Cart_shift(cart_comm, 1, 1, y_min_rank, y_max_rank, ierr)
 
     !Get my coordinates in Cartesian communicator
     CALL MPI_Cart_coords(cart_comm, rank, 2, coordinates, ierr)
 
     !Calculate what fraction of the global array this processor has
-    x_cell_min_local = nx_local * coordinates(2) + 1
-    x_cell_max_local = nx_local * (coordinates(2) + 1)
-    y_cell_min_local = ny_local * coordinates(1) + 1
-    y_cell_max_local = ny_local * (coordinates(1) + 1)
+    x_cell_min_local = nx_local * coordinates(1) + 1
+    x_cell_max_local = nx_local * (coordinates(1) + 1)
+    y_cell_min_local = ny_local * coordinates(2) + 1
+    y_cell_max_local = ny_local * (coordinates(2) + 1)
 
   END SUBROUTINE setup_mpi
 
